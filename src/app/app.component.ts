1a87e469da4c6bed61b0af2da9c7ca4a275d78fa
andrewbantly <andrewbantly@gmail.com>
2023-06-06 18:26:09 -0700

    getting started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `<h1>Hello World from inline template</h1>
+  // <p>Probably won't ever write html this way, but it's possible.</p>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hotelinventoryapp';
+  title = 'hotelinventoryheyapp';
 }
