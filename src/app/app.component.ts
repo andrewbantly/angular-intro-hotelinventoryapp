6c5ac0b7d0a393bc3770f4228345689378287407
andrewbantly <andrewbantly@gmail.com>
2023-06-08 15:23:11 -0700

    added room components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
   // template: `<h1>Hello World from inline template</h1>
   // <p>Probably won't ever write html this way, but it's possible.</p>`,
