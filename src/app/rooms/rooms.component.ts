a8cbb6506b5ade204a78e57dc4afff9cde5f076e
andrewbantly <andrewbantly@gmail.com>
2023-06-08 16:03:20 -0700

    added room data with ngFor and ngIf directives

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from "./rooms"
 
 @Component({
   selector: 'hinv-rooms',
@@ -11,6 +12,38 @@ export class RoomsComponent {
   numberOfRooms = 10;
 
   hideRooms = false;
+
+  rooms: Room = {
+    availableRooms: 10,
+    bookedRooms: 5,
+    totalRooms: 15,
+  }
+
+  roomList: RoomList[] = [{
+    roomNumber: 1,
+    roomType: 'Basic Room',
+    amenities: 'Air Conditioning, Wi-fi, TV, Bath Tub, Kitchenette',
+    price: 400,
+    photos: "https://www.acaciahotelsmanila.com/wp-content/uploads/2023/05/Rooms-and-Suites-AHM-1.jpg",
+    checkinTime: new Date('11-June-2023'),
+    checkoutTime: new Date('21-June-2023'),
+  }, {
+    roomNumber: 2,
+    roomType: 'Deluxe Room',
+    amenities: 'Air Conditioning, Wi-fi, TV, Bath Tub, Kitchenette',
+    price: 1000,
+    photos: "https://www.acaciahotelsmanila.com/wp-content/uploads/2023/05/Rooms-and-Suites-AHM-1.jpg",
+    checkinTime: new Date('11-June-2023'),
+    checkoutTime: new Date('21-June-2023'),
+  },{
+    roomNumber: 3,
+    roomType: 'Deluxe Suite',
+    amenities: 'Air Conditioning, Wi-fi, TV, Bath Tub, Kitchenette',
+    price: 1200,
+    photos: "https://www.acaciahotelsmanila.com/wp-content/uploads/2023/05/Rooms-and-Suites-AHM-1.jpg",
+    checkinTime: new Date('11-June-2023'),
+    checkoutTime: new Date('21-June-2023'),
+  }]
 
   toggle() {
     this.hideRooms = !this.hideRooms;
