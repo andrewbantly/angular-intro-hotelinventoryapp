f8327145e9090fb12559012331261431d9c959c4
andrewbantly <andrewbantly@gmail.com>
2023-06-09 11:52:11 -0700

    pipes

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -27,6 +27,7 @@ export class RoomsComponent {
     photos: "https://www.acaciahotelsmanila.com/wp-content/uploads/2023/05/Rooms-and-Suites-AHM-1.jpg",
     checkinTime: new Date('11-June-2023'),
     checkoutTime: new Date('21-June-2023'),
+    rating: 4.5,
   }, {
     roomNumber: 2,
     roomType: 'Deluxe Room',
@@ -35,6 +36,7 @@ export class RoomsComponent {
     photos: "https://www.acaciahotelsmanila.com/wp-content/uploads/2023/05/Rooms-and-Suites-AHM-1.jpg",
     checkinTime: new Date('11-June-2023'),
     checkoutTime: new Date('21-June-2023'),
+    rating: 4.28,
   },{
     roomNumber: 3,
     roomType: 'Deluxe Suite',
@@ -43,6 +45,7 @@ export class RoomsComponent {
     photos: "https://www.acaciahotelsmanila.com/wp-content/uploads/2023/05/Rooms-and-Suites-AHM-1.jpg",
     checkinTime: new Date('11-June-2023'),
     checkoutTime: new Date('21-June-2023'),
+    rating: 3.13,
   }]
 
   toggle() {
