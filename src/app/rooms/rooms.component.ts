f4394d8369b670f9cde6142be1877561a5ad81e5
andrewbantly <andrewbantly@gmail.com>
2023-06-15 16:37:54 -0700

    map operator

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -2,7 +2,7 @@ import { AfterViewInit, Component, DoCheck, OnInit, ViewChild, ViewChildren, Que
 import { Room, RoomList } from "./rooms"
 import { HeaderComponent } from '../header/header.component';
 import { RoomsService } from './services/rooms.service';
-import { Observable, Subject, Subscription, catchError, of } from 'rxjs';
+import { Observable, Subject, Subscription, catchError, map, of } from 'rxjs';
 import { HttpEventType } from '@angular/common/http';
 
 @Component({
@@ -64,6 +64,10 @@ export class RoomsComponent implements OnInit, DoCheck, AfterViewInit {
       return of([]);
     })
   );
+
+  roomsCount$  = this.roomsService.getRooms$.pipe(
+    map((rooms) => rooms.length) 
+  )
 
   subscription !: Subscription;
 
