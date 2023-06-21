24677ac320957fea7150f593b47c2d5b8cbc3077
andrewbantly <andrewbantly@gmail.com>
2023-06-21 16:00:05 -0700

    form adding rooms to backend successfully

--- src/app/rooms/rooms-add/rooms-add.component.ts
+++ src/app/rooms/rooms-add/rooms-add.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RoomList } from '../rooms';
+import { RoomsService } from '../services/rooms.service';
 
 @Component({
   selector: 'hinv-rooms-add',
   templateUrl: './rooms-add.component.html',
   styleUrls: ['./rooms-add.component.scss']
 })
-export class RoomsAddComponent {
+export class RoomsAddComponent implements OnInit {
+
+  room : RoomList = {
+    roomType: '',
+    amenities: '',
+    checkinTime: new Date(),
+    checkoutTime: new Date(),
+    photos: '',
+    price: 0,
+    rating: 0
+  }
+
+  successMessage:string = "";
+
+  constructor(private roomsService: RoomsService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  AddRoom() {
+    this.roomsService.addRoom(this.room).subscribe((data) => {
+     console.log(data)
+     this.successMessage = "Room added successfully."
+    })
+  }
 
 }
