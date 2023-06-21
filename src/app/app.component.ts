11f2a941c39fa07e5ecaeaa4d9c90875a8ca92a2
andrewbantly <andrewbantly@gmail.com>
2023-06-20 21:55:41 -0700

    code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { InitService } from './init.service';
   selector: 'hinv-root',
   templateUrl: './app.component.html',
   // template: `<h1>Hello World from inline template</h1>
-  // <p>Probably won't ever write html this way, but it's possible.</p>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
