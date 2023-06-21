f500efb66056ee1e285bc74fc6e4d32c64f90ac1
andrewbantly <andrewbantly@gmail.com>
2023-06-21 15:05:37 -0700

    import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { RoomsBookingComponent } from './rooms/rooms-booking/rooms-booking.component';
-import { RoomsAddComponent } from './rooms-add/rooms-add.component';
+import { RoomsAddComponent } from './rooms/rooms-add/rooms-add.component';
+import { FormsModule } from '@angular/forms';
 
 
 function initFactory(initService: InitService) {
@@ -52,7 +53,8 @@ function initFactory(initService: InitService) {
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    FormsModule
   ],
   providers: [ {
     provide: APP_SERVICE_CONFIG,
