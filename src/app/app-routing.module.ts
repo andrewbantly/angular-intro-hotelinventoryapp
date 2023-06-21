dbb5ef37a5d96b6b5bf1e7fadcb24287f21672e1
andrewbantly <andrewbantly@gmail.com>
2023-06-21 13:13:28 -0700

    added two routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // to configure a route, you need two things: a path and component
+  {
+    path: 'employee',
+    component: EmployeeComponent
+  },
+  {
+    path: 'rooms',
+    component: RoomsComponent
+  }
+];
 
 @NgModule({
+  // only configure forRoot once
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
