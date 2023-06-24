5e78ec014667702b87c6ced2f83539b7e8feb625
andrewbantly <andrewbantly@gmail.com>
2023-06-24 14:29:50 -0700

    basic hover directives

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[hinvHover]'
 })
-export class HoverDirective {
+export class HoverDirective implements OnInit {
 
-  constructor() { }
+  @Input() color: string = 'red';
+
+  constructor(private element: ElementRef, private renderer: Renderer2) {
+    console.log(this.element.nativeElement)
+   }
+
+  ngOnInit(): void {
+    console.log('Hover Directive')
+    // this.element.nativeElement.style.backgroundColor = this.color;
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', this.color)
+  };
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'blue')
+  }
 
 }
