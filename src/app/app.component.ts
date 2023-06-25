91843335171f0944f01e9a76eda1a63e92aa9625
andrewbantly <andrewbantly@gmail.com>
2023-06-25 15:37:21 -0700

    navigation event listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RoomsComponent } from './rooms/rooms.component';
 import { LocalStorageToken } from './localstorage.token';
 import { InitService } from './init.service';
 import { ConfigService } from './services/config.service';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'hinv-root',
@@ -19,18 +21,36 @@ export class AppComponent implements OnInit {
 
   @ViewChild('name', {static: true}) name!: ElementRef;
 
-  ngOnInit(): void {
-    this.name.nativeElement.innerText = "Hilton Hotel";
-    this.localStorage.setItem('name', 'Hilton Hotel');
-  }
-
+  
   constructor(
-      @Inject(LocalStorageToken) private localStorage: any, 
-      private initService: InitService,
-      private ConfigService: ConfigService) {
-    console.log(initService.config)
-  }
+    @Inject(LocalStorageToken) private localStorage: any, 
+    private initService: InitService,
+    private ConfigService: ConfigService,
+    private router: Router
+    ) {
+      console.log(initService.config)
+    }
+    
+    ngOnInit(): void {
+      // this.router.events.subscribe((event) => {
+      //   console.log(event)
+      // })
+      this.router.events.pipe(
+        filter((event) => event instanceof NavigationStart)
+      ).subscribe((event) => {
+        console.log('Navigation started')
+      })
+      this.router.events.pipe(
+        filter((event) => event instanceof NavigationEnd)
+      ).subscribe((event) => {
+        console.log('Navigation Completed')
+      })
 
+
+
+      this.name.nativeElement.innerText = "Hilton Hotel";
+      this.localStorage.setItem('name', 'Hilton Hotel');
+    }
   // ngAfterViewInit(): void {
   //   const componentRef = this.vcr.createComponent(RoomsComponent);
   //   componentRef.instance.numberOfRooms = 50;
