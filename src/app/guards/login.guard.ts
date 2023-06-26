dfb13956f0654efcb39978f6c7008e25d35c367a
andrewbantly <andrewbantly@gmail.com>
2023-06-25 17:02:09 -0700

    guard cleanup

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,7 +1,7 @@
 import { CanMatchFn, Route, UrlSegment } from '@angular/router';
-// import { LoginComponent } from '../login/login.component';
 import { inject } from '@angular/core';
 import { LoginService } from '../login/login.service';
+
 
 export const loginGuard: CanMatchFn = (route: Route, segments: UrlSegment[]): boolean => {
   console.log("here: ", inject(LoginService).isLoggedIn)
