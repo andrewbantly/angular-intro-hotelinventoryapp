cc3b96f428357f99aebcb91f9f8a25b5ad52b45d
andrewbantly <andrewbantly@gmail.com>
2023-06-26 20:30:05 -0700

    Booking form onInit

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ConfigService } from '../services/config.service';
 
 @Component({
   selector: 'hinv-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.scss']
 })
-export class BookingComponent {
+export class BookingComponent implements OnInit {
 
+  bookingForm!: FormGroup;
+
+  constructor(private configService: ConfigService, private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.bookingForm = this.fb.group({
+      roomId: [''],
+      guestEmail: [''],
+      checkinDate: [''],
+      checkoutDate: [''],
+      bookingStatus: [''],
+      bookingAmount: [''],
+      bookingDate: [''],
+      mobileNumber: [''],
+      guestName: [''],
+      guestAddress: [''],
+      guestCity: [''],
+      guestState: [''],
+      guestCountry: [''],
+      guestZipCode: [''],
+      guestCount: [''],
+      guestList: [''],
+    })
+  }
 }
 
 export class Booking {
