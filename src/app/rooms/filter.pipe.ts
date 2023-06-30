a958478356247831d29050cd58d7488da7519ad5
andrewbantly <andrewbantly@gmail.com>
2023-06-29 19:45:49 -0700

    bug fix on room filter

--- src/app/rooms/filter.pipe.ts
+++ src/app/rooms/filter.pipe.ts
@@ -1,12 +1,17 @@
+// DONT EVER CREATE A FRONT END FILTER, DO THIS ON YOUR BACK END. THIS IS JUST AN EXAMPLE
+
+
 import { Pipe, PipeTransform } from '@angular/core';
+import { RoomList } from './rooms';
+
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(rooms: RoomList[], price: number): RoomList[] {
+    return rooms.filter((room) => room.price <= price);
   }
 
 }
