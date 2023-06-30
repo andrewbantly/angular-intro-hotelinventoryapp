7ab81cf0bf3f6be0926425f1b3d69204db99092e
andrewbantly <andrewbantly@gmail.com>
2023-06-29 17:37:34 -0700

    Custom Validators

---
+++ src/app/booking/validators/customer-validator.spec.ts
@@ -0,0 +1,7 @@
+import { CustomerValidator } from './customer-validator';
+
+describe('CustomerValidator', () => {
+  it('should create an instance', () => {
+    expect(new CustomerValidator()).toBeTruthy();
+  });
+});
